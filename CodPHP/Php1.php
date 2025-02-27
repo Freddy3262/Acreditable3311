981044573a70def8ef1d6bb6a3a1c29c1b92e795
Freddy3262 <freddycanizales116@gmail.com>
2025-02-27 07:51:42 -0400

    Create Php1.php

---
+++ CodPHP/Php1.php
@@ -0,0 +1,13 @@
+class Persona {
+    public $nombre;
+    public $edad;
+
+    public function __construct($nombre, $edad) {
+        $this->nombre = $nombre;
+        $this->edad = $edad;
+    }
+
+    public function saludar() {
+        return "Hola, mi nombre es " . $this->nombre;
+    }
+}
